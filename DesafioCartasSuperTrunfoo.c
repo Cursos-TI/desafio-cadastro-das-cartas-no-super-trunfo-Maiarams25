b2ed36e1c5e99a623063a3f1d44621a515f2fcd2
Maiarams25 <maiaramanso154@gmail.com>
2025-04-22 15:43:32 +0000

    Comparação das cartas Vencedoras

--- DesafioCartasSuperTrunfoo.c
+++ DesafioCartasSuperTrunfoo.c
@@ -6,23 +6,25 @@ int main() {
     char estado1;
     int codigo1 [05]; 
     char cidade1[20];
-    int populacao1;
+    unsigned long int populacao1;
     float Area1;
     float PIB1;
     int PontosTuristico1;
     float DensidadePopulacional1;
     float PIBperCapita1;
+    float SuperPoder1;
 
     //Variaveis da segunda carta
     char estado2;
     int codigo2[05];
     char cidade2[20];
-    int populacao2;
+    unsigned long int populacao2;
     float Area2;
     float PIB2;
     int PontosTuristico2;
     float DensidadePopulacional2;
     float PIBperCapita2;
+    float SuperPoder2;
 
     printf("**BEM VINDO AO JOGO SUPER TRIUNFO**\n");
     printf("\n");
@@ -34,15 +36,15 @@ int main() {
     scanf("%c", &estado1);
 
     printf("Digite o codigo1: \n");
-    scanf("%s", &codigo1); 
+    scanf("%s", codigo1); 
 
     printf("Digite a cidade1: \n");
     scanf("%s", cidade1);
 
     printf("Digite a populacao1 \n");
-    scanf("%d", &populacao1);
+    scanf("%lu", &populacao1);
 
-    printf("Digite o Area1: \n");
+    printf("Digite o Area1 (em Km): \n");
     scanf("%f", &Area1);
 
     printf("Digite o PIB1: \n");
@@ -55,6 +57,8 @@ int main() {
     // CALCULOS DA CARTA 1 
     DensidadePopulacional1 = (float)populacao1 / Area1; 
     PIBperCapita1 = PIB1 / populacao1;
+    //Calculo do Super porder para Carta 1
+    SuperPoder1 = (float)populacao1 + Area1 + PIB1 + PontosTuristico1 + PIBperCapita1 + (DensidadePopulacional1 > 0 ? 1.0 / DensidadePopulacional1 : 0);
 
 
     // Entrada dos dados da segunda carta
@@ -63,15 +67,15 @@ int main() {
     scanf(" %c", &estado2);
 
     printf("Digite o codigo2: \n");
-    scanf("%s", &codigo2); 
+    scanf("%s", codigo2); 
 
     printf("Digite a cidade2: \n");
     scanf("%s", cidade2);
 
     printf("Digite a populacao2 \n");
-    scanf("%d", &populacao2);
+    scanf("%lu", &populacao2);
 
-    printf("Digite o Area2: \n");
+    printf("Digite o Area2 (em Km): \n");
     scanf("%f", &Area2);
 
     printf("Digite o PIB2: \n");
@@ -84,7 +88,8 @@ int main() {
     // CALCULOS DA CARTA 2  
     DensidadePopulacional2 = (float)populacao2 / Area2; 
     PIBperCapita2 = PIB2 / populacao2;
-
+    //Calculo do Super porder para Carta 2
+    SuperPoder2 = (float)populacao2 + Area2 + PIB2 + PontosTuristico2 + PIBperCapita2 + (DensidadePopulacional2 > 0 ? 1.0 / DensidadePopulacional2 : 0);
 
 
     // EXIBIÇÃO DOS DADOS DA PRIMEIRA CARTA
@@ -97,6 +102,7 @@ int main() {
     printf("Pontos Turistico: %d\n", PontosTuristico1);
     printf("Densidade Populacional: %.2f\n", DensidadePopulacional1); 
     printf("PIB per Capita: %.2f\n", PIBperCapita1);
+    printf("Super poder: %.2f\n", SuperPoder1);
     printf("\n");
 
 
@@ -110,9 +116,20 @@ int main() {
     printf("Pontos Turistico: %d\n", PontosTuristico2);
     printf("Densidade Populacional: %.2f\n", DensidadePopulacional2); 
     printf("PIB per Capita: %.2f\n", PIBperCapita2);
+    printf("Super poder: %.2f\n", SuperPoder2);
     printf("\n");
 
+    //Comparação das Cartas do Super Poder
+    printf("Comparação das Cartas:\n");
+    printf("\nVENCEDORAS:\n\n");
 
+    printf("População: Carta %d venceu (%d)\n", (populacao1 > populacao2) ? 1 : 2, (populacao1 > populacao2));
+    printf("Área: Carta %d venceu (%d)\n", (Area1 > Area2) ? 1 : 2, (Area1 > Area2));
+    printf("PIB: Carta %d venceu (%d)\n", (PIB1 > PIB2) ? 1 : 2, (PIB1 > PIB2));
+    printf("Pontos Turísticos: Carta %d venceu (%d)\n", (PontosTuristico1 > PontosTuristico2) ? 1 : 2, (PontosTuristico1 > PontosTuristico2));
+    printf("Densidade Populacional: Carta %d venceu (%d)\n", (DensidadePopulacional1 < DensidadePopulacional2) ? 1 : 2, (DensidadePopulacional1 < DensidadePopulacional2));
+    printf("PIB per Capita: Carta %d venceu (%d)\n", (PIBperCapita1 > PIBperCapita2) ? 1 : 2, (PIBperCapita1 > PIBperCapita2));
+    printf("Super Poder: Carta %d venceu (%d)\n", (SuperPoder1 > SuperPoder2) ? 1 : 2, (SuperPoder1 > SuperPoder2));
 
 
 
